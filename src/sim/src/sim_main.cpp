2f63fd3aeab4a9f06774fe52220cb7865816735e
roy1994 <dev.mancitrus@outlook.com>
2023-06-06 15:26:52 +0800

    init

---
+++ src/sim/src/sim_main.cpp
@@ -0,0 +1,26 @@
+#include <cstdint>
+#include <iostream>
+#include "Vtop.h"
+#include "verilated.h"
+#include <random>
+int main(int argc, char **argv) {
+    srand(time(0));
+    VerilatedContext *contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+    Vtop *top = new Vtop{contextp};
+    while (!contextp->gotFinish()) {
+        int a = (uint8_t)rand(), b = (uint8_t)rand();
+        top->a = a;
+        top->b = b;
+        top->eval();
+        if (top->o == (a ^ b)) {
+            std::cout << "OK" << std::endl;
+        } else {
+            std::cout << "Wrong" << std::endl;
+        }
+    }
+
+    delete top;
+    delete contextp;
+    return 0;
+}
