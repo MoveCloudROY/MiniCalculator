72df8d0c730e2c1a8c2d0dab8b3b64d45c4be409
roy1994 <dev.mancitrus@outlook.com>
2023-06-07 23:24:55 +0800

    feat(alu, sim): add some tools and fix some bugs in alu.v

--- src/sim/src/sim_main.cpp
+++ src/sim/src/sim_main.cpp
@@ -2,46 +2,93 @@
 #include <iostream>
 #include "Vtop.h"
 #include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "VcdWriter.h"
 #include "testFactory.h"
 #include <random>
+#include <bitset>
+
+using std::bitset;
+
 
 #define MAX_TEST 1000000
 
-// 遍历int8_t类型变量每一位并输出
-void printInt8(int8_t a) {
-    for (int i = 7; i >= 0; i--) {
-        std::cout << ((a >> i) & 0x01);
-    }
-    std::cout << std::endl;
-}
+// clang-format off
+#define print_err(fmt, args...) printf("\033[31;1m" fmt "\033[0m\n", ##args)
+#define print_info(fmt, args...) printf("\033[33;1m" fmt "\033[0m\n", ##args)
+#define debug(fmt, args...) printf("\033[32;1m" "[DEBUG] %s:%d:%s" "\033[0m  "  fmt "\n", __FILE__, __LINE__, __func__, ##args)
+// clang-format on
+
+
+#define BIT(x, n) bitset<(n)>(x).to_string().c_str()
 int main(int argc, char **argv) {
     srand(time(0));
+
     VerilatedContext *contextp = new VerilatedContext;
     contextp->commandArgs(argc, argv);
     Vtop *top = new Vtop{contextp};
 
+    VcdWriter vcdWriter("bin/top.vcd", contextp, top, 99);
+
     testFactory *tf    = new testFactory();
     int64_t      count = 0;
+    bool         clk   = 0;
+
+
     while (!contextp->gotFinish() && count < MAX_TEST) {
-        std::vector<int8_t> test = tf->genAddTest();
-        top->a                   = test[0];
-        top->b                   = test[1];
-        top->eval();
+
+        std::vector<int8_t> test = tf->genMulTest();
+
+        // reset
+        top->a   = 0;
+        top->b   = 0;
+        top->op  = 0;
+        top->clk = 0;
+        top->rst = 1;
+        vcdWriter.tick();
+        top->a   = 0;
+        top->b   = 0;
+        top->op  = 0;
+        top->clk = 1;
+        top->rst = 1;
+        vcdWriter.tick();
+        debug("RESET: top->o = %d top->busy = %d", top->o, top->busy);
+        // prepare data
+        top->a   = test[0];
+        top->b   = test[1];
+        top->op  = 2;
+        top->rst = 0;
+
+        top->clk = clk;
+        vcdWriter.tick();
+
+        clk      = !clk;
+        top->clk = clk;
+        vcdWriter.tick();
+
+        debug("INIT: top->a = %d top->b = %d top->busy = %d top->rst %d top->clk %d", top->a, top->b, top->busy, top->rst, top->clk);
+        while (top->busy) {
+            clk      = !clk;
+            top->clk = clk;
+            vcdWriter.tick();
+            debug("INSIDE: top->o = %d(0x%s)  top->clk %d", top->o, BIT(top->o, 8), top->clk);
+        }
+
         if (top->o == test[2]) {
             count++;
             if (count % 1000 == 0) {
                 std::cout << "PASS TEST: " << count << std::endl;
             }
         } else {
-            std::cout << "Wrong: " << std::endl;
-            std::cout << "op: ADD" << std::endl;
-            std::cout << "a: \t\t" << (int)test[0] << std::endl;
-            printInt8(test[0]);
-            std::cout << "b: \t\t" << (int)test[1] << std::endl;
-            printInt8(test[1]);
-            std::cout << "top->o\t\t: " << (int)top->o << std::endl;
-            std::cout << "answer: " << (int)test[2] << std::endl;
-            printInt8(test[2]);
+            print_err("======== Wrong ========");
+            print_info("Operator: MUL");
+            printf("Input a:\t\t %3d(0x%s) \n", test[0], BIT(test[0], 4));
+            // printInt8(test[0]);
+            printf("Input b:\t\t %3d(0x%s) \n", test[1], BIT(test[1], 4));
+            // printInt8(test[1]);
+            printf("Actually Answer:\t %3d(0x%s) \n", top->o, BIT(top->o, 8));
+            // printInt8(test[1]);
+            printf("Expected Answer:\t %3d(0x%s) \n", test[2], BIT(test[2], 8));
             exit(-1);
         }
     }
--- vsrc/alu.v
+++ vsrc/alu.v
@@ -4,14 +4,15 @@ module alu (
 
     // 1000 0100 0010 0001 0000
     //  +    -    *    /   STOP
-    input [3:0] op,
-    input [3:0] a,
-    input [3:0] b,
-    output busy,
-    output [7:0] o
+    input  [3:0] op,
+    input  [3:0] data1,
+    input  [3:0] data2,
+    output reg [7:0] o,
+    output reg       busy
 );
     reg reg_busy;
     reg [8:0] reg_o;
+    reg [3:0] reg_op;
     
     reg [2:0] reg_calc_cnt;
     reg [3:0] M, M_comp;
@@ -21,38 +22,40 @@ module alu (
         if (rst) begin
             reg_busy <= 1'b0;
             reg_o <= 9'd0;
+            reg_op <= 4'd0;
             reg_calc_cnt <= 3'd0;
             reg_step <= 2'd0;
-            {M, Q} <= {4'd0, 4'd0};
+            {M, M_comp} <= {4'd0, 4'd0};
         end
         
         case (op)
-            2'b1000: begin
-                tmp_o = a + b;
+            4'b1000: begin
+                reg_o[4:0] <= data1 + data2;
             end 
-            2'b0100: begin
-                tmp_o = a - b;
+            4'b0100: begin
+                reg_o[4:0] <= data1 - data2;
             end
-            2'b0010: begin
+            4'b0010: begin
                 case (reg_step)
-                    0: begin
-                        {M, M_comp} <= {a, ~a + 1'b0};
-                        reg_o <= {4'b0, b, 1'b0};
-                        reg_step <= reg_step + 1'b0;
-                        reg_calc_cnt <= reg_calc_cnt + 1'b0;
+                    2'd0: begin
+                        {M, M_comp} <= {data1, ~data1 + 1'b1};
+                        reg_o <= {4'b0, data2, 1'b0};
+                        reg_step <= reg_step + 1'b1;
+                        reg_calc_cnt <=  3'd0;
                         reg_busy <= 1'b1;
                     end 
-                    1: begin
+                    2'd1: begin
                         if (reg_calc_cnt == 3'd4) begin
+                            reg_busy <= 1'b0;
                             reg_o <= {1'b0, reg_o[8:1]};
                             reg_step <= reg_step + 2'd2;
                         end
                         else if(reg_o[1:0] == 2'b01) begin
-                            reg_o <= {reg_o[8:4] + M_comp, reg_o[3:0]};
+                            reg_o <= {reg_o[8:5] + M_comp, reg_o[4:0]};
                             reg_step <= reg_step + 1'd1;
                         end
                         else if(reg_o[1:0] == 2'b10) begin
-                            reg_o <= {reg_o[8:4] + M, reg_o[3:0]};
+                            reg_o <= {reg_o[8:5] + M, reg_o[4:0]};
                             reg_step <= reg_step + 1'd1;
                         end
                         else begin
@@ -60,18 +63,18 @@ module alu (
                         end
                         
                     end
-                    2: begin
-                        reg_o <= r{reg_o[16], reg_o[16:1]};
+                    2'd2: begin
+                        reg_o <= {reg_o[8], reg_o[8:1]};
                         reg_calc_cnt <= reg_calc_cnt + 1'd1;
                         reg_step <= reg_step - 1'd1;
                     end
-                    3: begin
-                        reg_busy <= 1'b0;
+                    2'd3: begin
+                        reg_step <= 2'd0;
                     end 
                 endcase
             end
 
-            2'b0001: begin
+            4'b0001: begin
                 
             end
 
@@ -80,8 +83,8 @@ module alu (
             end 
         endcase
         
-        assign busy = reg_busy;
-        assign o = reg_o[7:0];
     end
 
+    assign busy = reg_busy;
+    assign o = reg_o[7:0];
 endmodule
