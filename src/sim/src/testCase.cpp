85ba26448fcd3583e704d7dc25ce6caebcdf92c5
roy1994 <dev.mancitrus@outlook.com>
2023-06-08 17:07:44 +0800

    Merge

---
+++ src/sim/src/testCase.cpp
@@ -0,0 +1,53 @@
+#include "testCase.h"
+
+
+testCase::testCase(uint8_t opCode, std::vector<int8_t> params)
+{
+    this->opCode = opCode;
+    // 根据操作码设置
+    switch (opCode)
+    {
+    case 0:
+        this->op = "nop";
+        this->op1 = 0;
+        this->op2 = 0;
+        this->res1 = 0;
+        this->res2 = 0;
+        break;
+    case 8:
+        this->op = "add";
+        this->op1 = params[0];
+        this->op2 = params[1];
+        this->res1 = params[2];
+        this->res2 = 0;
+        break;
+    case 4:
+        this->op = "sub";
+        this->op1 = params[0];
+        this->op2 = params[1];
+        this->res1 = params[3];
+        this->res2 = 0;
+        break;
+    case 2:
+        this->op = "mul";
+        this->op1 = params[0];
+        this->op2 = params[1];
+        this->res1 = params[4];
+        this->res2 = params[5];
+        break;
+    case 1:
+        this->op = "div";
+        this->op1 = params[0];
+        this->op2 = params[1];
+        this->res1 = params[6];
+        this->res2 = params[7];
+        break;
+    default:
+        this->op = "error";
+        break;
+    }
+}
+
+testCase::~testCase()
+{
+}
--- src/sim/src/testFactory.cpp
+++ src/sim/src/testFactory.cpp
@@ -21,6 +21,18 @@ std::vector<int8_t> testFactory::genAddTest() {
     return test;
 }
 
+std::vector<int8_t> testFactory::genAddTest(int a, int b)
+{
+    std::vector<int8_t> test;
+    // 计算正确的结果, 高4位恒为0
+    int8_t sum = (a + b) & 0x0f; // 忽略溢出
+    // int8_t sum = (a + b); // 忽略溢出
+    test.push_back(a);
+    test.push_back(b);
+    test.push_back(sum);
+    return test;
+}
+
 // 生成减法测试
 std::vector<int8_t> testFactory::genSubTest() {
     std::vector<int8_t> test;
@@ -63,4 +75,29 @@ std::vector<int8_t> testFactory::genDivTest() {
     test.push_back(quot);
     test.push_back(rem);
     return test;
+}
+
+// 生成随机测试, nop表示是否包含nop测试
+testCase testFactory::genRandomTest(bool nop) {
+    int8_t a, b, sum, diff, prod_h, prod_l, quot, rem;
+
+    // 随机选择8, 4, 2, 1, 0中的一个作为操作码
+    uint8_t opCode = (1 << ( rand()% (nop ? 5 : 4) ) ) & 0x0f;
+    do
+    {
+        // 随机生成两个操作数a和b, 范围-8到7
+        a    = rand() % 16 - 8;
+        b    = rand() % 16 - 8;
+        sum  = (a + b) & 0x0f;
+        diff = a - b;
+        prod_h = (a * b)>>4; // 乘法高位
+        prod_l = a * b; // 乘法地位
+        quot = a / b;
+        rem  = a % b;
+    } while (opCode == 0x01 && b == 0); // 除法时除数不能为0
+
+    std::vector<int8_t> params = {a, b, sum, diff, prod_l, prod_h, quot, rem};
+    
+    return testCase(opCode, params);
+    
 }
