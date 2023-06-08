bafe50eb5f1ed95732e72794e6018a28d3ef198f
roy1994 <dev.mancitrus@outlook.com>
2023-06-08 17:11:08 +0800

    fix(test): fix some header bugs

--- src/sim/include/testCase.h
+++ src/sim/include/testCase.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstdint>
 #include <stdlib.h>
 #include <string>
 #include <vector>
@@ -10,17 +11,16 @@
 // 操作数1和操作数2为4位的有符号数, 可以使用int8存储
 // 正确的结果1和正确的结果2为4位的有符号数, 可以使用int8存储
 // 每个成员变量的低4位有效, 高4位为0
-class testCase
-{
+class testCase {
 private:
     /* data */
 public:
     std::string op; // 操作符
-    uint8_t opCode;
-    int8_t op1;
-    int8_t op2;
-    int8_t res1; // 加法, 减法的结果; 乘法的低位, 除法的商
-    int8_t res2; // 乘法的高位, 除法的余数
+    uint8_t     opCode;
+    int8_t      op1;
+    int8_t      op2;
+    int8_t      res1; // 加法, 减法的结果; 乘法的低位, 除法的商
+    int8_t      res2; // 乘法的高位, 除法的余数
     testCase(uint8_t opCode, std::vector<int8_t> params);
     ~testCase();
 };
--- src/sim/include/testFactory.h
+++ src/sim/include/testFactory.h
@@ -12,10 +12,9 @@
 // genSubTest方法中, 生成两个结果, 一个为差, 一个为借位
 // genMulTest方法中, 生成两个结果, 一个为积的高位, 一个为积的低位
 // genDivTest方法中, 生成两个结果, 一个为商, 一个为余数
-// 所有方法的生成结果均为int8, 但只有低4位有效, 高4位为0; 
+// 所有方法的生成结果均为int8, 但只有低4位有效, 高4位为0;
 // 这4个方法返回一个vector, 第一个元素为操作数1, 第二个元素为操作数2, 第三个元素为正确的结果
-class testFactory
-{
+class testFactory {
 private:
     /* data */
 public:
@@ -25,11 +24,6 @@ public:
     std::vector<int8_t> genAddTest(int a, int b);
     std::vector<int8_t> genSubTest();
     std::vector<int8_t> genMulTest();
-<<<<<<< HEAD
-    std::vector<int8_t> genDivTest();
-=======
-    testCase genDivTest();
->>>>>>> 8816057d3e3a5a191fca5111eabc68a5beb0d423
-    testCase genRandomTest(bool no);
+    testCase            genDivTest();
+    testCase            genRandomTest(bool no);
 };
-
